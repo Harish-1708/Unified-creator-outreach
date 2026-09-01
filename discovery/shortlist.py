01db53e958e96953b7c2590d986b256f770e3020
Harish-1708 <harish@kelson.agency>
2026-09-01 20:05:45 +0530

    Add new fields to shortlist data structure
    Added new fields for audience demographics and research confidence.

--- discovery/shortlist.py
+++ discovery/shortlist.py
@@ -35,6 +35,7 @@ SECTOR_HEADERS = [
     "niche_match", "audience_match", "location_match",
     "content_angle_strength", "partnership_signal_score", "overall_fit", "fit_explanation",
     "content_angle", "brand_affinity_note", "partnership_signal_matched", "competitor_affinity",
+    "dr_audience_gender", "dr_research_confidence", "dr_concerns",
     "outreach_readiness",
     "total_posts", "followers_count", "follower_verification", "follower_source",
     "engagement_rate", "posting_frequency", "audience_quality_score",
